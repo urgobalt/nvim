e72cb5c37e6340596d5ecf5c91768da3a55ca56f
Ludvig Källqvist Nygren <ludvigkallqvistnygren@gmail.com>
2024-08-11 13:05:21 +0000

    fix: color

--- lua/custom/plugins/color.lua
+++ lua/custom/plugins/color.lua
@@ -2,6 +2,20 @@ return {
 	{
 		"kvrohit/rasmus.nvim",
 		config = function()
+			vim.g.rasmus_italic_comments = false
+			vim.g.rasmus_bold_comments = false
+			vim.g.rasmus_italic_keywords = true
+			vim.g.rasmus_bold_keywords = false
+			vim.g.rasmus_italic_booleans = false
+			vim.g.rasmus_bold_booleans = false
+			vim.g.rasmus_italic_functions = false
+			vim.g.rasmus_bold_functions = true
+			vim.g.rasmus_italic_variables = false
+			vim.g.rasmus_bold_variables = false
+
+			vim.g.rasmus_transparent = true
+			vim.g.rasmus_variant = "dark"
+
 			vim.cmd.colorscheme("rasmus")
 		end,
 	},
