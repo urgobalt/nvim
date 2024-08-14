cfef5ac48b5e03e642cc8adc8e835e0d4d450f70
Ludvig Källqvist Nygren <ludvigkallqvistnygren@gmail.com>
2024-08-14 21:14:41 +0000

    fix: better looking fold signs

--- lua/custom/ufo.lua
+++ lua/custom/ufo.lua
@@ -3,6 +3,7 @@ vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decr
 vim.o.foldlevelstart = 99
 vim.o.foldmethod = "expr"
 vim.o.foldenable = true
+vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 
 vim.keymap.set("n", "zR", require("ufo").openAllFolds)
 vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
