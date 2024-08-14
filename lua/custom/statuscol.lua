57da7627c59cfa38066e66f880f4053d4de4714d
Ludvig Källqvist Nygren <ludvigkallqvistnygren@gmail.com>
2024-08-14 21:20:31 +0000

    feat: better statuscol management

---
+++ lua/custom/statuscol.lua
@@ -0,0 +1,57 @@
+local builtin = require("statuscol.builtin")
+function hello(hello) end
+require("statuscol").setup({
+	relculright = true,
+	setopt = true,
+	segments = {
+		{
+			sign = {
+				namespace = {
+					"vim_lsp_references",
+					"vim_lsp_semantic_tokens",
+					"vim_lsp_inlayhint",
+				},
+				maxwidth = 3,
+				colwidth = 1,
+				auto = false,
+				fillChar = "~",
+			},
+		},
+		{
+			sign = {
+				name = {
+					"Dap",
+					"neotest",
+				},
+				maxwidth = 2,
+				colwidth = 2,
+				auto = true,
+			},
+		},
+		{
+			sign = {
+				namespace = { "gitsign" },
+				maxwidth = 1,
+				colwidth = 1,
+				auto = false,
+				fillchar = " ",
+				fillcharhl = "StatusColumnSeparator",
+			},
+		},
+		{
+			text = { "%C ", builtin.lnumfunc, " " },
+		},
+	},
+	ft_ignore = {
+		"help",
+		"vim",
+		"fugitive",
+		"alpha",
+		"dashboard",
+		"neo-tree",
+		"Trouble",
+		"noice",
+		"lazy",
+		"toggleterm",
+	},
+})
