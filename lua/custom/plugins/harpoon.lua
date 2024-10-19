8dd007042c17b20692c3e4ca9432811040309a86
Ludvig Källqvist Nygren <ludvigkallqvistnygren@gmail.com>
2024-10-19 11:07:02 +0200

    format: indentation

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -1,10 +1,10 @@
 return {
-  "ThePrimeagen/harpoon",
-  branch = "harpoon2",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-  config = function()
-    require("custom.harpoon")
-  end,
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	config = function()
+		require("custom.harpoon")
+	end,
 }
