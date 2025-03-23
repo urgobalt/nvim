e45870899c36c5d43e81d557dc0eb64f29406478
Urgobalt <ludvigkallqvistnygren@gmail.com>
2025-03-23 15:44:09 +0100

    Fixed styling of neovim

--- lua/custom/plugins/color.lua
+++ lua/custom/plugins/color.lua
@@ -6,7 +6,7 @@ return {
     import = "huez-manager.import",
     config = function()
       require("huez").setup({
-        fallback = "rasmus",
+        fallback = "neobones",
         exclude = {
           "default",
           "minicyan",
@@ -32,25 +32,6 @@ return {
           "FoldColumn",
         },
       })
-    end,
-  },
-  {
-    "kvrohit/rasmus.nvim",
-    config = function()
-      vim.g.rasmus_italic_comments = false
-      vim.g.rasmus_bold_comments = false
-      vim.g.rasmus_italic_keywords = true
-      vim.g.rasmus_bold_keywords = false
-      vim.g.rasmus_italic_booleans = false
-      vim.g.rasmus_bold_booleans = false
-      vim.g.rasmus_italic_functions = false
-      vim.g.rasmus_bold_functions = true
-      vim.g.rasmus_italic_variables = false
-      vim.g.rasmus_bold_variables = false
-
-      vim.g.rasmus_variant = "dark"
-
-      -- vim.cmd.colorscheme("rasmus")
     end,
   },
   {
