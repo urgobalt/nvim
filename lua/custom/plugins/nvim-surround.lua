348703ae24eb55cedc7f446e585f2e60f8d9b52b
Urgobalt <ludvigkallqvistnygren@gmail.com>
2025-11-28 14:39:32 +0100

    change "<" and "(" behaviour for nvim-surround

--- lua/custom/plugins/nvim-surround.lua
+++ lua/custom/plugins/nvim-surround.lua
@@ -3,6 +3,15 @@ return {
   version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
   event = "VeryLazy",
   config = function()
-    require("nvim-surround").setup({})
+    require("nvim-surround").setup({
+      surrounds = {
+        ["("] = false,
+        ["<"] = false,
+      },
+      aliases = {
+        ["("] = ")",
+        ["<"] = ">",
+      },
+    })
   end,
 }
