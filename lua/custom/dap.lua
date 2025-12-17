0906b54e26c52cc3e5e4cd81c33dcb308da3b293
Urgobalt <ludvigkallqvistnygren@gmail.com>
2025-12-17 12:11:09 +0100

    dap ux update with support for asm

--- lua/custom/dap.lua
+++ lua/custom/dap.lua
@@ -4,6 +4,25 @@ local ui = require("dapui")
 ui.setup()
 require("dap-lldb").setup({})
 
+dap.configurations.asm = {
+  {
+    name = "Run executable (LLDB)",
+    type = "lldb",
+    request = "launch",
+    -- This requires special handling of 'run_last', see
+    -- https://github.com/mfussenegger/nvim-dap/issues/1025#issuecomment-1695852355
+    program = function()
+      local path = vim.fn.input({
+        prompt = "Path to executable: ",
+        default = vim.fn.getcwd() .. "/",
+        completion = "file",
+      })
+
+      return (path and path ~= "") and path or dap.ABORT
+    end,
+  },
+}
+
 vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint)
 vim.keymap.set("n", "<leader>gb", dap.run_to_cursor)
 
@@ -19,6 +38,11 @@ vim.keymap.set("n", "<F3>", dap.step_over)
 vim.keymap.set("n", "<F4>", dap.step_out)
 vim.keymap.set("n", "<F5>", dap.step_back)
 vim.keymap.set("n", "<F8>", dap.restart)
+
+vim.keymap.set("n", "<leader>d", function()
+  dap.close()
+  ui.close()
+end)
 
 dap.listeners.before.attach.dapui_config = function()
   ui.open()
