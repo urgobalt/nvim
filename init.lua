a61d99f03e145aee4825c17cf10e655f0e3cde75
Urgobalt <ludvigkallqvistnygren@gmail.com>
2026-01-18 09:49:11 +0100

    refactored file layout

--- init.lua
+++ init.lua
@@ -14,6 +14,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:append(lazypath)
 require("lazy").setup(
-  { import = "custom/plugins" },
+  { import = "plugins" },
   { change_detection = { notify = false } }
 )
